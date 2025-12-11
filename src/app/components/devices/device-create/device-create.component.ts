0dbd5c7a59b5fe624dbed03edad0bcc46afabd1d
voteros-mo <victor.oteros@masorange.es>
2025-12-11 12:06:41 +0100

    detail ready

--- src/app/components/devices/device-create/device-create.component.ts
+++ src/app/components/devices/device-create/device-create.component.ts
@@ -1,4 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { FormBuilder } from '@angular/forms';
+import { Router } from '@angular/router';
+import { DeviceService } from '../device.service';
 
 @Component({
   selector: 'app-device-create',
@@ -8,5 +11,36 @@ import { Component } from '@angular/core';
   styleUrl: './device-create.component.css'
 })
 export class DeviceCreateComponent {
+  private router = inject(Router);
+  private deviceService = inject(DeviceService)
 
+  private fb = inject(FormBuilder);
+  public deviceForm = this.fb.group({
+    name: ['', Validators.required],
+    description: ['', Validators.required],
+    price: [0, [Validators.required, Validators.min(1)]],
+    stock: [0, [Validators.required, Validators.min(0)]],
+  })
+
+  onSubmit() {
+    if (this.productForm.valid) {
+      const newProduct = this.productForm.value as {
+        name: string,
+        description: string,
+        price: number,
+        stock: number,
+      }
+
+      this.productService.addProduct(newProduct).subscribe({
+        next: () => {
+          console.log('Se ha creado el producto:', newProduct);
+          this.router.navigate(['/products'])
+        },
+        error: (error) => console.log('Se ha producido el error:', error)
+      });
+    }
+  }
+  onCancel() {
+    this.router.navigate(['/products'])
+  }
 }
--- src/app/components/devices/device-detail/device-detail.component.ts
+++ src/app/components/devices/device-detail/device-detail.component.ts
@@ -1,4 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, inject, signal } from '@angular/core';
+import { Device } from '../../../models/Device';
+import { ActivatedRoute } from '@angular/router';
+import { DeviceStore } from '../device-store';
 
 @Component({
   selector: 'app-device-detail',
@@ -8,5 +11,14 @@ import { Component } from '@angular/core';
   styleUrl: './device-detail.component.css'
 })
 export class DeviceDetailComponent {
+  device = signal<Device | undefined>(undefined);
+  
+  private route = inject(ActivatedRoute);
+  private store = inject(DeviceStore);
 
+  ngOnInit(): void {
+    const id = this.route.snapshot.paramMap.get('id');
+    const deviceFound = this.store.devices().find((d: { id: number | null; })  => d.id === id);
+    this.device.set(deviceFound);
+  }
 }
